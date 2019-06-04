6eff926ed4ea116c97836bf7854ef93ab82f8e39
Nicholas Gabble <48841487+ngabble@users.noreply.github.com>
2019-06-04 08:41:50 -0700

    Update SinglyLinkedList.cs
    Added a way to check if the list is a palindrome

--- Day05/SinglyLinkedList.cs
+++ Day05/SinglyLinkedList.cs
@@ -8,21 +8,48 @@ namespace CSC395_Module3
 {
     class Program
     {
-        static void Main(string[] args) // O(n)
+        static void Main(string[] args) // O(n^2)
         {
             // Instantiate a new linked list object and populate it with strings. Then print it to prove it works with strings. 
             SinglyLinkedList myList = new SinglyLinkedList();
-            myList.Insert("Albert");
-            myList.Insert("Fred");
-            myList.Insert("Bob");
-            myList.Insert("Derrik");
-            myList.Insert("Chad");
-            myList.Insert("Garth");
-            myList.Insert("Zelda"); 
+            myList.Insert("Albert"); // O(n)
+            myList.Insert("Albert"); // O(n)
+            myList.Insert("Fred"); // O(n)
+            myList.Insert("Bob"); // O(n)
+            myList.Insert("Derrik"); // O(n)
+            myList.Insert("Bob"); // O(n)
+            myList.Insert("Chad"); // O(n)
+            myList.Insert("Garth"); // O(n)
+            myList.Insert("Fred"); // O(n)
+            myList.Insert("Zelda"); // O(n)
+            myList.Insert("Zelda"); // O(n)
+            myList.AddLast("Albert"); // O(1)
 
-            myList.PrintList();
+            myList.PrintList(); // O(n)
 
-            Console.ReadLine();
+            myList.RemoveDublicates(); // O(n^2)
+
+            myList.PrintList(); // O(n)
+
+            bool answer = myList.IsPalindrome(); //O(1)
+
+            Console.WriteLine(); //O(1)
+            Console.WriteLine($"Is the list a palindrome? {answer}"); //O(1)
+
+            SinglyLinkedList myList2 = new SinglyLinkedList(); //O(1)
+            myList2.AddFirst("Church"); //O(1)
+            myList2.AddFirst("Monk"); //O(1)
+            myList2.AddLast("Church"); //O(1)
+            myList2.AddLast("Monk"); //O(1)
+
+            myList2.PrintList(); //O(n)
+
+            answer = myList2.IsPalindrome(); //O(n)
+
+            Console.WriteLine(); //O(1)
+            Console.WriteLine($"Is the list a palindrome? {answer}"); //O(1)
+
+            Console.ReadLine(); // O(?)
         }
     }
     // A Node class with a single pointer.
@@ -74,7 +101,7 @@ namespace CSC395_Module3
             {
                 Node curr = first; // O(1)
 
-                while (curr.next != null &&  curr.next.value != val) // O(n)
+                while (curr.next != null && curr.next.value != val) // O(n)
                     curr = curr.next; // O(1)
                 if (curr.next == null) // O(1)
                     throw new Exception("we didn't find  the element in the list"); // O(1)
@@ -91,7 +118,7 @@ namespace CSC395_Module3
             {
                 Node newNode = new Node(val); // O(1)
                 Node curr = first; // O(1)
-                while (curr.next!=null && curr.next.value.CompareTo(val) < 0) // O(n)
+                while (curr.next != null && curr.next.value.CompareTo(val) < 0) // O(n)
                     curr = curr.next; // O(1)
 
                 newNode.next = curr.next; // O(1)
@@ -112,7 +139,7 @@ namespace CSC395_Module3
         {
             if (IsEmpty()) // O(1)
                 throw new IndexOutOfRangeException("you can't remove last from an emtpy list"); // O(1)
-            else if(first.next == null) // O(1)
+            else if (first.next == null) // O(1)
             {
                 first = null; // O(1)
             }
@@ -147,6 +174,87 @@ namespace CSC395_Module3
                     current = current.next; // O(1)
                 }
             }
+        }
+        // First we check if the list is empty, assuming it's not we'll create two pointers and compare them iteratively, moving comparer through the remainder of the list
+        // each time we move curr foreward once. If we find a duplicate we link the duplicate out and run the comparison again without moving the pointers.
+        public void RemoveDublicates() // O(n^2)
+        {
+            if (IsEmpty()) // O(1)
+            {
+                Console.WriteLine("the list is empty!!!"); // O(1)
+            }
+
+            Node curr = first; // O(1)
+            Node comparer = first; // O(1)
+
+            while (curr != null && curr.next != null)  // O(n^2)
+            {
+                if (comparer == null) // O(1)
+                {
+                    curr = curr.next; // O(1)
+                    comparer = curr; // O(1)
+                }
+                if (comparer != null && comparer.next != null && curr.value.CompareTo(comparer.next.value) == 0) // O(1)
+                {
+                    comparer.next = comparer.next.next; // O(1)
+                    continue; // O(1)
+                }
+                if (comparer != null) // O(1)
+                    comparer = comparer.next; // O(1)
+            }
+        }
+        // This is the function we want to call to check for a palindrom. It creates two pointers and moves both to the last node in the list. 
+        // It then starts a loop, putting one pointer to the node just before the other pointer while moving the head pointer forward and comparing them. 
+        // The loop breaks if it finds two values that aren't equal and returns false. Otherwise it compares each element until the pointers meet in the middle.
+        public bool IsPalindrome()  // O(n)
+        {
+            bool answer = true; // O(1)
+            if (IsEmpty()) // O(1)
+                Console.WriteLine("the list is empty!!!"); // O(1)
+
+            Node head = first;  // O(1)
+            Node comparer1 = first;  // O(1)
+            
+            
+            while (comparer1.next != null)  // O(n)
+                comparer1 = comparer1.next;  // O(1)
+
+            Node comparer2 = comparer1; // O(1)
+
+            while (head != comparer1 && head != comparer2 && head.next != comparer1 && head.next != comparer2) // O(n)
+            {
+                if (head.next != null && head.next != comparer1 && head.next != comparer2) // O(1)
+                {
+                    if (head.value.CompareTo(comparer1.value) != 0) // O(1)
+                    {
+                        answer = false; // O(1)
+                        break; // O(1)
+                    }
+               
+                    head = head.next; // O(1)
+                    comparer2 = head; // O(1)
+                    while (comparer2.next != comparer1)  // O(n)
+                    {
+                        comparer2 = comparer2.next;  // O(1)
+                    }
+                }
+                if (head.next != null && head.next != comparer1 && head.next != comparer2) // O(1)
+                {
+                    if (head.value.CompareTo(comparer2.value) != 0) // O(1)
+                    {
+                        answer = false; // O(1)
+                        break; // O(1)
+                    }
+                
+                    head = head.next; // O(1)
+                    comparer1 = head; // O(1)
+                    while (comparer1.next != comparer2)  // O(n)
+                    {
+                        comparer1 = comparer1.next;  // O(1)
+                    }
+                }
+            }
+            return answer;  // O(1)
         }
         // A default constructor.
         public SinglyLinkedList() // O(1)
